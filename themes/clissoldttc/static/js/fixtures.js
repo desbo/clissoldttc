cfe60308312e3a87c57a81121e48ddce5879c19c
Sam Desborough <sam.desborough@gmail.com>
2021-10-10 11:48:23 +0100

    Update fixtures.js

--- themes/clissoldttc/static/js/fixtures.js
+++ themes/clissoldttc/static/js/fixtures.js
@@ -1,63 +1,38 @@
-const divisionIDs = [
-  1134, 1135, 1136, 1137, 1138, 1139, 1140
-]
+const apiURL = 'https://tt365-fixtures.appspot.com';
+const league = 'CentralLondon';
+const season = 'Winter_2021-22';
+const clubId = 5123;
 
-function upcomingMatchesURL(division, from) {
-  return `https://centrallondon.ttleagues.com/v1/api/divisions/${division}/matches/upcoming?_=${from}`
-}
-
-function matches(url) {
-  return fetch(url)
-    .then(r => r.json())
-    .then(json => json.matches)
-}
-
-function testTeams(f, match) {
-  return f(match.home) || f(match.away)
-}
-
-function involvesTeam(match, teamName) {
-  return testTeams(t => t.name.toLowerCase().includes(teamName), match)
-}
-
-function time(match) {
-  return new Date(Date.parse(match.time || match.date))
-}
+const url = `${apiURL}/${league}/${season}?club_id=${clubId}`
 
 function init() {
-  const now = new Date()
-  const matchData = divisionIDs.map(id => matches(upcomingMatchesURL(id, now)))
+  fetch(url)
+    .then(res => res.json())
+    .then(fixtures => {
+      const upcoming = fixtures.filter(f => new Date(f.time) > new Date());
+      const matchesHtml = upcoming.map(match => {
+        const time = new Date(match.time);
+        const dateString = time.toUTCString().replace(' GMT', '')
 
-  now.setDate(now.getDate() - 1) // ensures matches for the current day are shown
-  
-  Promise.all(matchData)
-    .then(matches => {
-      const matchesHtml = matches.flat()
-        .filter(m => involvesTeam(m, "clissold") && !involvesTeam(m, "bye") && !!time(m).getDate() && time(m) >= now)
-        .sort((a, b) => new Date(a.date) - new Date(b.date))
-        .map(match => {
-          const dateString = time(match).toDateString().replace(' GMT', '')
-
-          return `
-            <div class="match">
-              <p>
-                <div class="time">${dateString} – <em>${match.name}</em></div>
-
-                <div class="teams">
-                  <h2 class="title is-4">
-                    <span class="team">${match.home.name}</span> vs <span class="team">${match.away.name}</span>
-                  </h2>
-                </div>
-
-                <div class="location">
-                  at <span class="location__name">${match.venue}</span>
-                </div>
-              </p>
-            </div>
-            `
-        })
+        return `
+          <div class="match">
+            <p>
+              <div class="time">${dateString}</div>
+              <div class="teams">
+                <h2 class="title is-4">
+                  <span class="team">${match.home.name}</span> vs <span class="team">${match.away.name}</span>
+                </h2>
+              </div>
+              <div class="location">
+                at <span class="location__name">${match.venue}</span>
+              </div>
+            </p>
+          </div>
+          `
+      });
 
       const html = matchesHtml.length > 0 ? matchesHtml.join('\n') : '<p>No matches scheduled at the moment.</p>'
+
       document.getElementById('fixtures').insertAdjacentHTML('afterbegin', html);
     });
 }
